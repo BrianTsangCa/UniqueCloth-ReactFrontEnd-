ab9cc79eb89c7743219507ff41f947ecfde85074
BrianTsangCa <110198947+BrianTsangCa@users.noreply.github.com>
2024-05-02 15:17:45 -0700

    components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import Login from "./components/Login";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" exact element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
